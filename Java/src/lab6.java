820d0b8367917c2c1867dd9e5546e87ed15188c0
Vasya-556 <vkobrin556@gmail.com>
2023-10-05 20:20:28 +0300

    Add

--- Java/src/lab6.java
+++ Java/src/lab6.java
@@ -1,15 +1,16 @@
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.function.BiPredicate;
 import java.util.regex.Pattern;
 
-public class lab5 {
-    static MyLinkedList5<Book5> books = LoadList();
+public class lab6 {
+    static List<Book6> books = LoadList();
     public static void main(String[] args) {
         if (args.length > 0 && args[0].equals("-auto")){
-            Book5 book = new Book5(
+            Book6 book = new Book6(
                     "00000",
                     "AutoName",
                     "AutoPublish",
@@ -18,17 +19,21 @@ public class lab5 {
             );
             book.AddAuthor("AutoAuthor");
             books.add(book);
-            Display5.PrintList(books);
+            Display6();
             DisplaySpecificBook();
             SaveBook();
             return;
         }
-        while (true) {
+        while (true){
             System.out.println("Menu:");
             System.out.println("1. Add a new book");
             System.out.println("2. View list of books");
             System.out.println("3. Delete book by name");
             System.out.println("4. Display specific books");
+            System.out.println("5. Sort by Name");
+            System.out.println("6. Sort by Publish");
+            System.out.println("7. Sort by Genre");
+            System.out.println("8. Sort by Date");
             System.out.println("0. Exit program");
             System.out.print("Select an option: ");
 
@@ -40,6 +45,7 @@ public class lab5 {
                 case 0:
                     SaveBook();
                     System.out.println("Program terminated.");
+                    System.exit(0);
                     break;
 
                 case 1:
@@ -47,7 +53,7 @@ public class lab5 {
                     break;
 
                 case 2:
-                    Display5.PrintList(books);
+                    Display6();
                     break;
 
                 case 3:
@@ -58,32 +64,48 @@ public class lab5 {
                     DisplaySpecificBook();
                     break;
 
+                case 5:
+                    SortByName();
+                    break;
+
+                case 6:
+                    SortByPublish();
+                    break;
+
+                case 7:
+                    SortByGenre();
+                    break;
+
+                case 8:
+                    SortByDate();
+                    break;
+
                 default:
                     System.out.println("Invalid choice");
                     break;
             }
         }
     }
-    public static MyLinkedList5<Book5> LoadList() {
-        MyLinkedList5<Book5> books;
+    public static List<Book6> LoadList() {
+        List<Book6> books;
         File file = new File("books.json");
         if (file.exists()) {
             try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
-                Object object = ois.readObject();
-                if (object instanceof MyLinkedList5<?>) {
-                    books = (MyLinkedList5<Book5>) object;
+                Object obj = ois.readObject();
+                if (obj instanceof List<?>) {
+                    books = (List<Book6>) obj;
                     System.out.println("Data loaded successfully from books.json");
                 } else {
-                    System.err.println("books.json does not contain a valid list of books.");
-                    books = new MyLinkedList5<>();
+                    System.err.println("Invalid data format in books.json");
+                    books = new ArrayList<>();
                 }
             } catch (IOException | ClassNotFoundException e) {
                 System.err.println("Error loading data from books.json: " + e.getMessage());
-                books = new MyLinkedList5<>();
+                books = new ArrayList<>();
             }
         } else {
             System.out.println("Starting with an empty list.");
-            books = new MyLinkedList5<>();
+            books = new ArrayList<>();
         }
         return books;
     }
@@ -117,7 +139,7 @@ public class lab5 {
             }
         }
 
-        Book5 book = new Book5(ISBN, Name, Publish, Genre, Date);
+        Book6 book = new Book6(ISBN, Name, Publish, Genre, Date);
 
         while (true){
             System.out.print("Add author (Y/N): ");
@@ -132,110 +154,57 @@ public class lab5 {
         }
         books.add(book);
     }
+    public static void SaveBook(){
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("books.json"))){
+            oos.writeObject(books);
+            System.out.println("Data successfully saved to file");
+        }
+        catch (IOException e){
+            System.err.println("Error saving data to file: " + e.getMessage());
+        }
+    }
+    public static void Display6(){
+        for (Book6 book : books){
+            System.out.println(book);
+        }
+    }
     public static void RemoveBook(){
-        System.out.print("Enter book name(s): ");
+        System.out.println("Enter book name(s): ");
         Scanner scanner = new Scanner(System.in);
         String BookName = scanner.nextLine();
         books.removeIf(book -> book.getName().equalsIgnoreCase(BookName));
         System.out.println("Book(s) with name " + BookName + " deleted (if found).");
     }
-    public static void SaveBook(){
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("books.json"))){
-            oos.writeObject(books);
-            System.out.println();
-        }
-        catch (IOException e){
-            System.err.println("Error saving data to file: " + e.getMessage());
-        }
-        System.out.println("Program terminated");
-        System.exit(0);
-    }
     public static void DisplaySpecificBook(){
         System.out.println("Specific book: ");
-        for (Book5 book : books) {
+        for (Book6 book : books) {
             if (book.HasSpecificAuthor() && book.HasSpecificISBN() && book.HasSpecificAuthor()){
                 System.out.println(book);
             }
         }
     }
-}
-
-class Node5<T> implements Serializable {
-    T data;
-    Node5<T> next;
-
-    Node5(T data) {
-        this.data = data;
-        this.next = null;
+    public static void SortByName(){
+        books.sort(Comparator.comparing(Book6::getName));
+    }
+    public static void SortByPublish(){
+        books.sort(Comparator.comparing(Book6::getPublish));
+    }
+    public static void SortByGenre(){
+        books.sort(Comparator.comparing(Book6::getGenre));
+    }
+    public static void SortByDate(){
+        books.sort(Comparator.comparing(Book6::getDate));
     }
 }
 
-class MyLinkedList5<T> implements Iterable<T>, Serializable {
-    private Node5<T> head = null;
-
-    public void add(T item) {
-        Node5<T> newNode = new Node5<>(item);
-        if (head == null) {
-            head = newNode;
-        } else {
-            Node5<T> temp = head;
-            while (temp.next != null) {
-                temp = temp.next;
-            }
-            temp.next = newNode;
-        }
-    }
-
-    public Iterator<T> iterator() {
-        return new Iterator<T>() {
-            private Node5<T> current = head;
-
-            public boolean hasNext() {
-                return current != null;
-            }
-
-            public T next() {
-                T data = current.data;
-                current = current.next;
-                return data;
-            }
-        };
-    }
-
-    public void removeIf(java.util.function.Predicate<T> predicate) {
-        if (head == null) return;
-
-        while (head != null && predicate.test(head.data)) {
-            head = head.next;
-        }
-
-        Node5<T> current = head;
-        while (current != null && current.next != null) {
-            if (predicate.test(current.next.data)) {
-                current.next = current.next.next;
-            } else {
-                current = current.next;
-            }
-        }
-    }
-}
-
-class Display5 {
-    public static <T> void PrintList(MyLinkedList5<T> list){
-        for (T item : list){
-            System.out.println(item);
-        }
-    }
-}
-
-class Book5 implements Serializable {
+class Book6 implements Serializable {
     private String ISBN;
     private String Name;
     private List<String> Authors;
     private String Publish;
     private String Genre;
     private String Date;
-    public Book5(String ISBN, String Name, String Publish, String Genre, String Date){
+    public Book6(String ISBN, String Name, String Publish, String Genre, String Date){
         this.ISBN = ISBN;
         this.Name = Name;
         this.Authors = new ArrayList<>();
@@ -245,6 +214,15 @@ class Book5 implements Serializable {
     }
     public String getName() {
         return this.Name;
+    }
+    public String getPublish() {
+        return this.Publish;
+    }
+    public String getGenre() {
+        return this.Genre;
+    }
+    public String getDate() {
+        return this.Date;
     }
     public void AddAuthor(String Author) {
         Authors.add(Author);
