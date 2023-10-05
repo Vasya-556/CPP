cad57d5ab740cab175b01d70e952e28a0a46bfdd
Vasya-556 <vkobrin556@gmail.com>
2023-10-05 20:19:49 +0300

    Add

--- Java/src/lab5.java
+++ Java/src/lab5.java
@@ -3,13 +3,13 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
-public class lab4 {
-    static MyLinkedList<Book4> books = LoadList();
-    public static void main(String[] args){
-
+public class lab5 {
+    static MyLinkedList5<Book5> books = LoadList();
+    public static void main(String[] args) {
         if (args.length > 0 && args[0].equals("-auto")){
-            Book4 book = new Book4(
+            Book5 book = new Book5(
                     "00000",
                     "AutoName",
                     "AutoPublish",
@@ -18,16 +18,17 @@ public class lab4 {
             );
             book.AddAuthor("AutoAuthor");
             books.add(book);
-            Display.PrintList(books);
+            Display5.PrintList(books);
+            DisplaySpecificBook();
             SaveBook();
             return;
         }
-
         while (true) {
             System.out.println("Menu:");
             System.out.println("1. Add a new book");
             System.out.println("2. View list of books");
             System.out.println("3. Delete book by name");
+            System.out.println("4. Display specific books");
             System.out.println("0. Exit program");
             System.out.print("Select an option: ");
 
@@ -46,10 +47,15 @@ public class lab4 {
                     break;
 
                 case 2:
-                    Display.PrintList(books);
+                    Display5.PrintList(books);
                     break;
+
                 case 3:
                     RemoveBook();
+                    break;
+
+                case 4:
+                    DisplaySpecificBook();
                     break;
 
                 default:
@@ -58,26 +64,26 @@ public class lab4 {
             }
         }
     }
-    public static MyLinkedList<Book4> LoadList() {
-        MyLinkedList<Book4> books;
+    public static MyLinkedList5<Book5> LoadList() {
+        MyLinkedList5<Book5> books;
         File file = new File("books.json");
         if (file.exists()) {
             try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
                 Object object = ois.readObject();
-                if (object instanceof MyLinkedList<?>) {
-                    books = (MyLinkedList<Book4>) object;
+                if (object instanceof MyLinkedList5<?>) {
+                    books = (MyLinkedList5<Book5>) object;
                     System.out.println("Data loaded successfully from books.json");
                 } else {
                     System.err.println("books.json does not contain a valid list of books.");
-                    books = new MyLinkedList<>();
+                    books = new MyLinkedList5<>();
                 }
             } catch (IOException | ClassNotFoundException e) {
                 System.err.println("Error loading data from books.json: " + e.getMessage());
-                books = new MyLinkedList<>();
+                books = new MyLinkedList5<>();
             }
         } else {
             System.out.println("Starting with an empty list.");
-            books = new MyLinkedList<>();
+            books = new MyLinkedList5<>();
         }
         return books;
     }
@@ -98,10 +104,20 @@ public class lab4 {
         System.out.print("Genre: ");
         String Genre = scanner.nextLine();
 
-        System.out.print("Date: ");
-        String Date = scanner.nextLine();
+        String Date;
+        while (true){
+            System.out.print("Date (DD-MM-YYYY): ");
+            Date = scanner.nextLine();
+            String pattern = "^(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-\\d{4}$";
 
-        Book4 book = new Book4(ISBN, Name, Publish, Genre, Date);
+            if (Pattern.matches(pattern, Date)) {
+                break;
+            } else {
+                System.out.println("Wrong data format");
+            }
+        }
+
+        Book5 book = new Book5(ISBN, Name, Publish, Genre, Date);
 
         while (true){
             System.out.print("Add author (Y/N): ");
@@ -134,27 +150,35 @@ public class lab4 {
         System.out.println("Program terminated");
         System.exit(0);
     }
+    public static void DisplaySpecificBook(){
+        System.out.println("Specific book: ");
+        for (Book5 book : books) {
+            if (book.HasSpecificAuthor() && book.HasSpecificISBN() && book.HasSpecificAuthor()){
+                System.out.println(book);
+            }
+        }
+    }
 }
 
-class Node<T> implements Serializable {
+class Node5<T> implements Serializable {
     T data;
-    Node<T> next;
+    Node5<T> next;
 
-    Node(T data) {
+    Node5(T data) {
         this.data = data;
         this.next = null;
     }
 }
 
-class MyLinkedList<T> implements Iterable<T>, Serializable {
-    private Node<T> head = null;
+class MyLinkedList5<T> implements Iterable<T>, Serializable {
+    private Node5<T> head = null;
 
     public void add(T item) {
-        Node<T> newNode = new Node<>(item);
+        Node5<T> newNode = new Node5<>(item);
         if (head == null) {
             head = newNode;
         } else {
-            Node<T> temp = head;
+            Node5<T> temp = head;
             while (temp.next != null) {
                 temp = temp.next;
             }
@@ -164,7 +188,7 @@ class MyLinkedList<T> implements Iterable<T>, Serializable {
 
     public Iterator<T> iterator() {
         return new Iterator<T>() {
-            private Node<T> current = head;
+            private Node5<T> current = head;
 
             public boolean hasNext() {
                 return current != null;
@@ -185,7 +209,7 @@ class MyLinkedList<T> implements Iterable<T>, Serializable {
             head = head.next;
         }
 
-        Node<T> current = head;
+        Node5<T> current = head;
         while (current != null && current.next != null) {
             if (predicate.test(current.next.data)) {
                 current.next = current.next.next;
@@ -196,23 +220,22 @@ class MyLinkedList<T> implements Iterable<T>, Serializable {
     }
 }
 
-class Display {
-    public static <T> void PrintList(MyLinkedList<T> list){
+class Display5 {
+    public static <T> void PrintList(MyLinkedList5<T> list){
         for (T item : list){
             System.out.println(item);
         }
     }
 }
 
-class Book4 implements  Serializable {
+class Book5 implements Serializable {
     private String ISBN;
     private String Name;
     private List<String> Authors;
     private String Publish;
     private String Genre;
     private String Date;
-
-    public Book4(String ISBN, String Name, String Publish, String Genre, String Date){
+    public Book5(String ISBN, String Name, String Publish, String Genre, String Date){
         this.ISBN = ISBN;
         this.Name = Name;
         this.Authors = new ArrayList<>();
@@ -236,5 +259,19 @@ class Book4 implements  Serializable {
                 "Genre: '" + Genre + "', "+
                 "Date: '" + Date + "'";
     }
+    public boolean HasSpecificAuthor(){
+        return Authors.contains("Й. Й.");
+    }
+    public boolean HasSpecificISBN(){
+        String pattern = ".*9\\d9\\d9.*";
+        return Pattern.matches(pattern, ISBN);
+    }
+    public boolean HasSpecificYear() {
+        int currentYear = java.time.Year.now().getValue();
+        String pattern = String.format(
+                "^\\d{2}-\\d{2}-%d|\\d{2}-\\d{2}-%d|\\d{2}-\\d{2}-%d|\\d{2}-\\d{2}-%d|\\d{2}-\\d{2}-%d$",
+                currentYear - 4, currentYear - 3, currentYear - 2, currentYear - 1, currentYear
+        );
+        return Pattern.matches(pattern, Date);
+    }
 }
-
